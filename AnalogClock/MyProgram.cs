eb04c927c1997c0510ba3c9dcd4de6d28507d9fd
LinusNTI <92534409+LinusNTI@users.noreply.github.com>
2022-01-13 14:21:21 +0100

    Optimizations

--- AnalogClock/MyProgram.cs
+++ AnalogClock/MyProgram.cs
@@ -1,49 +1,96 @@
 using System;
+using System.Drawing;
 using System.Threading;
 
 namespace AnalogClock
 {
     class MyProgram
     {
-        private bool _isRunning = true;
+        private bool isRunning = true;
 
         private float hourPointerScale = 0.5f;
         private float minutePointerScale = 0.7f;
         private float secondPointerScale = 0.95f;
 
-        private int FPS = 3;
+        private int fps = 5;
+
+        private DateTime oldTime = DateTime.Now;
+        private Size oldSize = new Size(Console.WindowWidth, Console.WindowHeight);
+
+        bool hasRenderedSphere = false;
+
+        Size GetWindowSize()
+        {
+            return new Size(Console.WindowWidth, Console.WindowHeight);
+        }
+
+        public PointF GetCirclePosition(float radius, float degrees) => new PointF((float)(radius * Math.Sin(Math.PI * 2 * degrees / 360)), (float)(radius * Math.Cos(Math.PI * 2 * degrees / 360)));
 
         public void Run()
         {
-            while (_isRunning)
+            while (isRunning)
             {
                 // Clear screen, also assuming that it starts at 0,0 since while loop should end with setting cursor pos to 0
                 // Why not use Console.Clear()? because it's dogshit and causes flickering :anger:
-                for (int y = 0; y < Console.WindowHeight; y++)
+                if (GetWindowSize() != oldSize)
                 {
-                    Console.WriteLine(new string(' ', Console.WindowWidth));
-                }
-                Console.SetCursorPosition(0, 0);
+                    for (int y = 0; y < Console.WindowHeight; y++)
+                    {
+                        Console.WriteLine(new string(' ', Console.WindowWidth));
+                    }
+                    Console.SetCursorPosition(0, 0);
 
-                
+                    hasRenderedSphere = false;
+                }
+
                 int maxSize = (Console.WindowHeight < Console.WindowWidth / 2 ? Console.WindowHeight : Console.WindowWidth / 2) / 2;
                 DateTime timeNow = DateTime.Now;
 
-                #if DEBUG // We only want to write this as debug information
-                Console.WriteLine($"{timeNow.Hour}:{timeNow.Minute}:{timeNow.Second}");
-                #endif
-
                 #region Draw Sphere
-                int perimiter = (int)Math.Round(Math.PI * maxSize * 2);
-                float degreesPerUnit = (float)360 / (float)perimiter;
-                for (int i = 0; i < perimiter; i++)
+                if (!hasRenderedSphere)
                 {
-                    // Calculation for X and Y source from here: https://stackoverflow.com/a/43642346
-                    double x = maxSize * Math.Sin(Math.PI * 2 * (degreesPerUnit * i) / 360);
-                    double y = maxSize * Math.Cos(Math.PI * 2 * (degreesPerUnit * i) / 360);
+                    int perimiter = (int)Math.Round(Math.PI * maxSize * 2);
+                    float degreesPerUnit = (float)360 / (float)perimiter;
+                    for (int i = 0; i < perimiter; i++)
+                    {
+                        PointF positionOnCircle = GetCirclePosition(maxSize, degreesPerUnit * i);
 
-                    Console.SetCursorPosition((maxSize*2)+(int)Math.Round(x*2), maxSize+(int)Math.Round(y));
-                    Console.Write("#");
+                        Console.SetCursorPosition((maxSize * 2) + (int)Math.Round(positionOnCircle.X * 2), maxSize + (int)Math.Round(positionOnCircle.Y));
+                        Console.Write("#");
+                    }
+                    hasRenderedSphere = true;
+                }
+                #endregion
+
+                #region Remove old pointers
+                int secondLength2 = (int)Math.Round(maxSize * secondPointerScale);
+                for (int i = 0; i < secondLength2; i++)
+                {
+                    double secondsToDegrees = 180 - ((360 / 60) * (oldTime.Second + (float)oldTime.Millisecond / 1000f));
+                    PointF positionOnCircle = GetCirclePosition(i, (float)secondsToDegrees);
+
+                    Console.SetCursorPosition((maxSize * 2) + (int)Math.Round(positionOnCircle.X * 2), maxSize + (int)Math.Round(positionOnCircle.Y));
+                    Console.Write(" ");
+                }
+
+                int minuteLength2 = (int)Math.Round(maxSize * minutePointerScale);
+                for (int i = 0; i < minuteLength2; i++)
+                {
+                    double minutesToDegrees = 180 - ((360 / 60) * (oldTime.Minute + (float)oldTime.Second / 60f));
+                    PointF positionOnCircle = GetCirclePosition(i, (float)minutesToDegrees);
+
+                    Console.SetCursorPosition((maxSize * 2) + (int)Math.Round(positionOnCircle.X * 2), maxSize + (int)Math.Round(positionOnCircle.Y));
+                    Console.Write(" ");
+                }
+
+                int hourLength2 = (int)Math.Round(maxSize * hourPointerScale);
+                for (int i = 0; i < hourLength2; i++)
+                {
+                    double hoursToDegrees = 180 - ((360 / 12) * (oldTime.Hour + (float)oldTime.Minute / 60f));
+                    PointF positionOnCircle = GetCirclePosition(i, (float)hoursToDegrees);
+
+                    Console.SetCursorPosition((maxSize * 2) + (int)Math.Round(positionOnCircle.X * 2), maxSize + (int)Math.Round(positionOnCircle.Y));
+                    Console.Write(" ");
                 }
                 #endregion
 
@@ -52,11 +99,9 @@ namespace AnalogClock
                 for (int i = 0; i < secondLength; i++)
                 {
                     double secondsToDegrees = 180 - ((360 / 60) * (timeNow.Second + (float)timeNow.Millisecond / 1000f));
+                    PointF positionOnCircle = GetCirclePosition(i, (float)secondsToDegrees);
 
-                    double x = i * Math.Sin(Math.PI * 2 * secondsToDegrees / 360);
-                    double y = i * Math.Cos(Math.PI * 2 * secondsToDegrees / 360);
-
-                    Console.SetCursorPosition((maxSize * 2) + (int)Math.Round(x * 2), maxSize + (int)Math.Round(y));
+                    Console.SetCursorPosition((maxSize * 2) + (int)Math.Round(positionOnCircle.X * 2), maxSize + (int)Math.Round(positionOnCircle.Y));
                     Console.Write(".");
                 }
 
@@ -64,11 +109,9 @@ namespace AnalogClock
                 for (int i = 0; i < minuteLength; i++)
                 {
                     double minutesToDegrees = 180 - ((360 / 60) * (timeNow.Minute + (float)timeNow.Second / 60f));
+                    PointF positionOnCircle = GetCirclePosition(i, (float)minutesToDegrees);
 
-                    double x = i * Math.Sin(Math.PI * 2 * minutesToDegrees / 360);
-                    double y = i * Math.Cos(Math.PI * 2 * minutesToDegrees / 360);
-
-                    Console.SetCursorPosition((maxSize * 2) + (int)Math.Round(x * 2), maxSize + (int)Math.Round(y));
+                    Console.SetCursorPosition((maxSize * 2) + (int)Math.Round(positionOnCircle.X * 2), maxSize + (int)Math.Round(positionOnCircle.Y));
                     Console.Write("*");
                 }
 
@@ -76,34 +119,21 @@ namespace AnalogClock
                 for (int i = 0; i < hourLength; i++)
                 {
                     double hoursToDegrees = 180 - ((360 / 12) * (timeNow.Hour + (float)timeNow.Minute / 60f));
-                    
+                    PointF positionOnCircle = GetCirclePosition(i, (float)hoursToDegrees);
 
-                    double x = i * Math.Sin(Math.PI * 2 * hoursToDegrees / 360);
-                    double y = i * Math.Cos(Math.PI * 2 * hoursToDegrees / 360);
-
-                    Console.SetCursorPosition((maxSize * 2) + (int)Math.Round(x * 2), maxSize + (int)Math.Round(y));
+                    Console.SetCursorPosition((maxSize * 2) + (int)Math.Round(positionOnCircle.X * 2), maxSize + (int)Math.Round(positionOnCircle.Y));
                     Console.Write("@");
                 }
                 #endregion
 
-                #if DEBUG // Write numbers on the clock
-                for (int i = 1; i < 13; i++)
-                {
-                    double degreesPerNum = 360 / 12;
+                oldTime = timeNow;
+                oldSize = GetWindowSize();
 
-                    double x = maxSize * Math.Sin(Math.PI * 2 * (180 - (degreesPerNum * i)) / 360);
-                    double y = maxSize * Math.Cos(Math.PI * 2 * (180 - (degreesPerNum * i)) / 360);
-
-                    Console.SetCursorPosition((maxSize * 2) + (int)Math.Round(x * 2), maxSize + (int)Math.Round(y));
-                    Console.Write(i);
-                }
-                #endif
-
-                Console.SetCursorPosition(maxSize*2, maxSize);
+                Console.SetCursorPosition(maxSize * 2, maxSize);
                 Console.Write("O"); // Write knob in centre
                 Console.SetCursorPosition(0, 0); // Reset position so that ugly thing isn't right where I'm looking...
 
-                Thread.Sleep(1000/FPS); // FPS conversion
+                Thread.Sleep(1000 / fps); // FPS conversion
             }
         }
     }
